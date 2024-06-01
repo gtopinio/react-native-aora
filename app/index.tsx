c962c90edb244c6b9bb9a76c88bb2dd467ada592
gtopinio <mctopinio@up.edu.ph>
2024-06-02 00:57:12 +0800

    update: add status bar to onboarding page screen

--- app/index.tsx
+++ app/index.tsx
@@ -60,7 +60,10 @@ export default function Index() {
           </CustomButton>
         </View>
       </ScrollView>
-
+      <StatusBar
+        backgroundColor="#161622"
+        style="light"
+      />
     </SafeAreaView>
   );
 }
