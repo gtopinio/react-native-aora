3f787c9b0d42b85420317d46b699f71bc4094ccd
gtopinio <mctopinio@up.edu.ph>
2024-06-01 14:21:03 +0800

    update: play around with new route and stack screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
+const RootLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen 
+        name="index"
+        options={{
+          headerShown: false,
+        }} 
+      />
+      <Stack.Screen 
+        name="profile"
+        options={{
+          title: "Profile Page",
+        }}
+      />
     </Stack>
-  );
+  )
 }
+
+export default RootLayout
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.appHeader}>Welcome to Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href={'/profile'} style={styles.profile}>Go to Profile</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appHeader: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  profile : {
+    color: "blue",
+    fontSize: 20
+  }
+})
