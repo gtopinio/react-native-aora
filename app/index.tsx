b2d25f60205b7d92ab938c38567a5f441e6ec57e
gtopinio <mctopinio@up.edu.ph>
2024-06-01 16:16:20 +0800

    fix: add more ts configuration for nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
 export default function Index() {
   return (
     <View
-      style={styles.container}
+      className="flex-1 items-center justify-center bg-blue-100"
     >
-      <Text style={styles.appHeader}>Welcome to Aora!</Text>
+      <Text className="text-2xl font-bold">Welcome to Aora!</Text>
       <StatusBar style="auto" />
-      <Link href={'/profile'} style={styles.profile}>Go to Profile</Link>
+      <Link className="mt-5 underline text-blue-900" href={'/profile'}>Go to Profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  appHeader: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  profile : {
-    color: "blue",
-    fontSize: 20
-  }
-})
