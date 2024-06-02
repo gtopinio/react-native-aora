c8603540eafae85fca39cddb116f02a20d6cd605
gtopinio <mctopinio@up.edu.ph>
2024-06-02 20:39:30 +0800

    update: implement sign in screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
+import images from '@/constants/images'
 
 const SignIn = () => {
     return (
-        <View>
-            <Text>SignIn</Text>
-        </View>
+        <SafeAreaView
+            className='bg-primary h-full'
+        >
+            <ScrollView>
+                <View
+                    className='w-full justify-center h-full px-4 my-6'
+                >
+                    <Image
+                        source={images.logo}
+                        resizeMode='contain'
+                        className='w-[115px] h-[35px]'
+                    />
+                    <Text
+                        className='text-white text-2xl font-semibold mt-10 font-psemibold'
+                    >
+                        Log in to Aora
+                    </Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
