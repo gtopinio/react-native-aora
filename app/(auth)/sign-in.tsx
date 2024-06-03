3f54b26eb08464fbec61da544a3859f72b95c1a1
gtopinio <mctopinio@up.edu.ph>
2024-06-03 13:46:45 +0800

    update: add sign in and sign up links in sign in component

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -4,12 +4,20 @@ import { useState } from 'react'
 import React from 'react'
 import images from '@/constants/images'
 import FormField from '@/components/FormField'
+import CustomButton from '@/components/CustomButton'
+import { Link } from 'expo-router'
 
 const SignIn = () => {
     const [form, setForm] = useState({
         email: '',
         password: '',
-    })
+    });
+
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const submitForm = () => {
+
+    }
 
     return (
         <SafeAreaView
@@ -33,15 +41,36 @@ const SignIn = () => {
                         title="Email"
                         value={form.email}
                         handleChangeText={(e: any) => setForm({ ...form, email: e })}
-                        otherStyles='mt-7'
+                        otherStyles={`mt-7`}
                         keyboardType='email-address'
                     />
                     <FormField
                         title="Password"
                         value={form.password}
                         handleChangeText={(e: any) => setForm({ ...form, password: e })}
-                        otherStyles='mt-7'
+                        otherStyles={`mt-7`}
                     />
+                    <CustomButton
+                        title='Sign In'
+                        handlePress={submitForm}
+                        containerStyle={`mt-7`}
+                        isLoading={isSubmitting}
+                    />
+                    <View
+                        className='justify-center pt-5 flex-row gap-2'
+                    >
+                        <Text
+                            className='text-lg text-gray-100 font-pregular'
+                        >
+                            Don't have an account?
+                        </Text>
+                        <Link
+                            className='text-lg text-secondary font-psemibold'
+                            href={'sign-up'}
+                        >
+                            Sign Up
+                        </Link>
+                    </View>
                 </View>
             </ScrollView>
         </SafeAreaView>
