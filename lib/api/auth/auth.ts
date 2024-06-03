e8445bea3935245fa3a3d0e4cc208c9a946b2904
gtopinio <mctopinio@up.edu.ph>
2024-06-03 15:33:56 +0800

    update: add proper logic for creating users and sign in api

--- lib/api/auth/auth.ts
+++ lib/api/auth/auth.ts
@@ -1,22 +1,60 @@
-import client from "@/lib/appwrite";
-import { Account, ID } from "react-native-appwrite";
+import client, { config } from "@/lib/appwrite";
+import { Account, Avatars, Databases, ID } from "react-native-appwrite";
 
 const account = new Account(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
 
-export const createUser = (
+export const createUser = async (
     username: string,
     email: string,
     password: string,
 ) => {
-    account.create(
-        ID.unique(),
-        email,
-        password,
-        username
-    ).then(function (response) {
-        console.log(response);
-    }, function (error) {
+    try {
+        const newAccount = await account.create(
+            ID.unique(),
+            email,
+            password,
+            username
+        );
+
+        if (!newAccount) {
+            throw new Error("Account not created");
+        }
+
+        const avatarUrl = avatars.getInitials(username);
+
+        await signIn(email, password);
+
+        const newUser = await databases.createDocument(
+            config.databaseId,
+            config.userCollectionId,
+            ID.unique(),
+            {
+                accountId: newAccount.$id,
+                email,
+                username,
+                avatar: avatarUrl,
+            }
+        );
+
+        return newUser;
+        
+    } catch (error) {
         console.log(error);
+        throw new Error(String(error));
     }
-    )
+}
+
+export const signIn = async (
+    email: string,
+    password: string,
+) => {
+    try {
+        const session = await account.createSession(email, password);
+        return session;
+    } catch (error) {
+        console.log(error);
+        throw new Error(String(error));
+    }
 }
