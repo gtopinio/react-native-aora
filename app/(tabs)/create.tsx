683adf82cd22c6d2f9f7cf1e40d0223bb709df83
gtopinio <mctopinio@up.edu.ph>
2024-06-09 22:30:05 +0800

    update: add loading state when submitting a post

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, TouchableOpacity, Image, Alert } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, Image, Alert, ActivityIndicator } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import FormField from '@/components/FormField'
@@ -80,109 +80,122 @@ const Create = () => {
         <SafeAreaView
             className='bg-primary h-full'
         >
-            <ScrollView // Holds the entire form fields
-                className='px-4 my-6'
-            >
-                <Text
-                    className='text-2xl font-psemibold text-white'
-                >
-                    Upload Video
-                </Text>
-
-                <FormField 
-                    title='Video Title'
-                    value={form.title}
-                    placeholder='Give your video a catchy title'
-                    handleChangeText={(e) => setform({ ...form, title: e })} // We spread the form while updating the title
-                    otherStyles='mt-10'
-                />
-
-                <View
-                    className='mt-7 space-y-2'
-                >
-                    <Text
-                        className='text-base text-gray-100 font-pmedium'
-                    >
-                        Upload Video
-                    </Text>
-                    <TouchableOpacity
-                        onPress={() => openPicker('video')}
-                    >
-                        {form.video ? ( 
-                            <Video
-                                source={{ uri: form.video.uri }}
-                                className='w-full h-64 rounded-2xl'
-                                resizeMode={ResizeMode.COVER}
+                {
+                    uploading ? (
+                        <View
+                            className='flex-1 justify-center items-center'
+                        >
+                            <ActivityIndicator
+                                size='large'
+                                color='#FFA001'
                             />
-                        ) : (
-                            <View
-                                className='w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center'
+                        </View>
+                    ) : (
+                        <ScrollView // Holds the entire form fields
+                            className='px-4 my-6'
+                        >
+                            <Text
+                                className='text-2xl font-psemibold text-white'
                             >
-                                <View
-                                    className='w-14 h-14 border border-dashed border-secondary-100 justify-center items-center'
-                                >
-                                    <Image
-                                        source={icons.upload}
-                                        resizeMode='contain'
-                                        className='w-1/2 h-1/2'
-                                    />
-                                </View>
-                            </View>
-                        )}
-                    </TouchableOpacity>
-                </View>
+                                Upload Video
+                            </Text>
 
-                <View
-                    className='mt-7 space-y-2'
-                >
-                    <Text
-                        className='text-base text-gray-100 font-pmedium'
-                    >
-                        Thumbnail Image
-                    </Text>
-                    <TouchableOpacity
-                        onPress={() => openPicker('image')}
-                    >
-                        {form.thumbnail ? ( 
-                            <Image
-                                source={{ uri: form.thumbnail.uri }}
-                                className='w-full h-64 rounded-2xl'
-                                resizeMode='cover'
+                            <FormField 
+                                title='Video Title'
+                                value={form.title}
+                                placeholder='Give your video a catchy title'
+                                handleChangeText={(e) => setform({ ...form, title: e })} // We spread the form while updating the title
+                                otherStyles='mt-10'
                             />
-                        ) : (
+
                             <View
-                                className='w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-200 flex-row space-x-2'
+                                className='mt-7 space-y-2'
                             >
-                                <Image
-                                    source={icons.upload}
-                                    resizeMode='contain'
-                                    className='w-5 h-5'
-                                />
                                 <Text
-                                    className='text-sm text-gray-100 font-pmedium'
+                                    className='text-base text-gray-100 font-pmedium'
                                 >
-                                    Choose a File
+                                    Upload Video
                                 </Text>
+                                <TouchableOpacity
+                                    onPress={() => openPicker('video')}
+                                >
+                                    {form.video ? ( 
+                                        <Video
+                                            source={{ uri: form.video.uri }}
+                                            className='w-full h-64 rounded-2xl'
+                                            resizeMode={ResizeMode.COVER}
+                                        />
+                                    ) : (
+                                        <View
+                                            className='w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center'
+                                        >
+                                            <View
+                                                className='w-14 h-14 border border-dashed border-secondary-100 justify-center items-center'
+                                            >
+                                                <Image
+                                                    source={icons.upload}
+                                                    resizeMode='contain'
+                                                    className='w-1/2 h-1/2'
+                                                />
+                                            </View>
+                                        </View>
+                                    )}
+                                </TouchableOpacity>
                             </View>
-                        )}
-                    </TouchableOpacity>
-                </View>
 
-                <FormField 
-                    title='AI Prompt'
-                    value={form.prompt}
-                    placeholder='Prompt used to generate the video'
-                    handleChangeText={(e) => setform({ ...form, prompt: e })} // We spread the form while updating the title
-                    otherStyles='mt-7'
-                />
+                            <View
+                                className='mt-7 space-y-2'
+                            >
+                                <Text
+                                    className='text-base text-gray-100 font-pmedium'
+                                >
+                                    Thumbnail Image
+                                </Text>
+                                <TouchableOpacity
+                                    onPress={() => openPicker('image')}
+                                >
+                                    {form.thumbnail ? ( 
+                                        <Image
+                                            source={{ uri: form.thumbnail.uri }}
+                                            className='w-full h-64 rounded-2xl'
+                                            resizeMode='cover'
+                                        />
+                                    ) : (
+                                        <View
+                                            className='w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-200 flex-row space-x-2'
+                                        >
+                                            <Image
+                                                source={icons.upload}
+                                                resizeMode='contain'
+                                                className='w-5 h-5'
+                                            />
+                                            <Text
+                                                className='text-sm text-gray-100 font-pmedium'
+                                            >
+                                                Choose a File
+                                            </Text>
+                                        </View>
+                                    )}
+                                </TouchableOpacity>
+                            </View>
 
-                <CustomButton
-                    title='Submit & Publish'
-                    handlePress={submit}
-                    containerStyle='mt-7'
-                    isLoading={uploading}
-                />
-            </ScrollView>
+                            <FormField 
+                                title='AI Prompt'
+                                value={form.prompt}
+                                placeholder='Prompt used to generate the video'
+                                handleChangeText={(e) => setform({ ...form, prompt: e })} // We spread the form while updating the title
+                                otherStyles='mt-7'
+                            />
+
+                            <CustomButton
+                                title='Submit & Publish'
+                                handlePress={submit}
+                                containerStyle='mt-7'
+                                isLoading={uploading}
+                            />
+                        </ScrollView>
+                    )
+                }
         </SafeAreaView>
     )
 }
